f04b08e2cb8dcd713461a891eb57ad6b1692e955
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 01:19:51 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,19 @@
-def hello_t(["Tim","Tom","Jim"])
+def hello_t(array)
+  i=0
+  while i<array.length
+    yield array[i]
+    i+=1
+  end
+end
+
+hello_t(["Tim","Tom","Jim"]) do
+  |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+
+
 
 # call your method here!
 
